9829000c5b69243b3fa4dbfdac60ef5e36b0d293
Alonso Isidoro Román <alonsoir@gmail.com>
2025-08-05 17:31:21 +0200

    Modified protocols/current/__init__.py to import protocols.current.network_event_extended_v3_pb2

--- protocols/current/__init__.py
+++ protocols/current/__init__.py
@@ -3,5 +3,6 @@
 from . import network_event_extended_fixed_pb2
 from . import firewall_commands_pb2
 from . import network_event_pb2
+from . import network_event_extended_v3_pb2
 
-__all__ = ["network_event_pb2","network_event_extended_fixed_pb2","firewall_commands_pb2"]
+__all__ = ["network_event_pb2","network_event_extended_fixed_pb2","network_event_extended_v3_pb2","firewall_commands_pb2"]
